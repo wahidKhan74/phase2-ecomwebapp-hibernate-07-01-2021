96030f0dde7de4805f4d20303ce83d8e03cdbbc7
Waheed Khan <wahid.khan74@gmail.com>
2021-07-01 07:33:35 +0000

    add create and list product

---
+++ WebContent/product_table.sql
@@ -0,0 +1,3 @@
+use ecomwebapp_db;
+create table eproduct_data(id bigint primary key auto_increment, name varchar(100), price decimal(10,2),
+created_at timestamp default now(), modified_at timestamp default now());
---
+++ src/com/dell/webapp/util/HibernateSessionUtil.java
@@ -0,0 +1,24 @@
+package com.dell.webapp.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateSessionUtil {
+	
+	private static SessionFactory factory;
+	
+	public static SessionFactory buildSessionFactory() {
+		// load configuration
+		StandardServiceRegistry ssRegistry = new StandardServiceRegistryBuilder()
+				.configure("hibernate.cfg.xml").build();
+		
+		Metadata metadata = new MetadataSources(ssRegistry).getMetadataBuilder().build();
+		
+		factory = metadata.getSessionFactoryBuilder().build();
+		
+		return factory;
+	}
+}
